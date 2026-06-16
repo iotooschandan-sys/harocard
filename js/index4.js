5bb2de9fe828a0305bd182e84d00617a718986f8
chandan kumar <iotoos.chandan@gmail.com>
2026-06-16 20:09:17 +0530

    new

--- js/index4.js
+++ js/index4.js
@@ -1,6 +1,4 @@
 
- 
- 
 
 const cards = document.querySelectorAll(".service-card");
 const modal = document.getElementById("popupModal");
@@ -45,32 +43,4 @@ modal.addEventListener("click",(e)=>{
         modal.classList.remove("active");
     }
 
-});
-
-
-//   <!-- Initialize Swiper -->
-var swiper = new Swiper(".portfolio-swiper", {
-    loop:true,
-    navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-    },
-
-    breakpoints: {
-        320: {
-            spaceBetween: 10, // Mobile
-        },
-        576: {
-            spaceBetween: 20, // Small devices
-        },
-        768: {
-            spaceBetween: 40, // Tablet
-        },
-        992: {
-            spaceBetween: 60, // Laptop
-        },
-        1200: {
-            spaceBetween: 80, // Desktop
-        }
-    }
 });
