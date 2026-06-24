1b8fb8f421217b68576a707a0d7fa0f6b083abd5
chandan kumar <iotoos.chandan@gmail.com>
2026-06-24 17:18:52 +0530

    js

--- js/index.js
+++ js/index.js
@@ -1,19 +1,50 @@
 
+ 
 
-document.querySelector(".close-btn")
-.addEventListener("click",()=>{
+// const cards = document.querySelectorAll(".service-card");
+// const modal = document.getElementById("popupModal");
 
-    modal.classList.remove("active");
+// const popupTitle = document.getElementById("popupTitle");
+// const popupContent = document.getElementById("popupContent");
+// const popupImg = document.getElementById("popupImg");
 
-});
+// cards.forEach(card=>{
 
-modal.addEventListener("click",(e)=>{
+//     card.addEventListener("click",()=>{
 
-    if(e.target===modal){
-        modal.classList.remove("active");
-    }
+//         const title =
+//         card.querySelector("h2,h3").innerText;
 
-});
+//         const content =
+//         card.querySelector("p").innerText;
+
+//         const image =
+//         card.querySelector("img").src;
+
+//         popupTitle.innerText = title;
+//         popupContent.innerText = content;
+//         popupImg.src = image;
+
+//         modal.classList.add("active");
+
+//     });
+
+// });
+
+// document.querySelector(".close-btn")
+// .addEventListener("click",()=>{
+
+//     modal.classList.remove("active");
+
+// });
+
+// modal.addEventListener("click",(e)=>{
+
+//     if(e.target===modal){
+//         modal.classList.remove("active");
+//     }
+
+// });
 
 
 //   <!-- Initialize Swiper -->
